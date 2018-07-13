ea5778aceb66534e51744cb087507c0a7c0721e7
Zach Gallagher <zachagallagher@gmail.com>
2018-07-13 10:25:35 -0700

    Passing by output

--- Program.cs
+++ Program.cs
@@ -12,12 +12,26 @@ namespace learn_csharp
             Console.WriteLine("Hello, {0}", name);
         }
 
+        // Passing by reference
         void Swap(ref int x, ref int y)
         {
             int temp;
             temp = x;
             x = y;
             y = temp;
+        }
+
+        // Passing by output
+        static void GetValues(out int x, out int y)
+        {
+            x = 8;
+            y = 24;
+        }
+        static void TestGetValues(string[] args)
+        {
+            int a, b;
+            GetValues(out a, out b);
+            // Now a equals 8, b equals 24
         }
     }
 }
